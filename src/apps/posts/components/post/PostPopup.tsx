bb2beca56f83ad120dec27e57b0b698928d090d3
tien.ly <tien.ly@terralogic.com>
2023-01-24 21:39:02 +0700

    Add post component (pending)

---
+++ src/apps/posts/components/post/PostPopup.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PostPopup = () => {
+  return (
+    <div>PostPopup</div>
+  )
+}
--- src/base/theme/index.ts
+++ src/base/theme/index.ts
@@ -6,7 +6,8 @@ export type IScreen = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
 export type ISpacing = 'none' | 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type IZIndex = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type IBorderColors = 'light' | 'main' | 'dark';
-export type Colors =
+export type IGreyColors = 5 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90;
+export type IColors =
   | 'primary'
   | 'secondary'
   | 'success'
